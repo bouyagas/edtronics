5f668fe8a45287a731bf3dfa84ecf21a05f067b0
mohamed gassama <bouyagassama@gmail.cpm>
2017-01-24 21:23:38 -0500

    updates [Edtronics] files

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const express = require ('express');
 const logger = require ('morgan');
 const path = require ('path');
 const bodyParser = require ('body-parser');
+const colors = require('colors/safe');
+
+colors.setTheme({
+  info: ['white', 'bgBlack', 'underline', 'italic' ]
+});
 
 const server = express();
 const PORT = process.argv[2] || process.env.PORT || 3001;
@@ -14,7 +19,7 @@ server.use(bodyParser.json());
 
 server.use(express.static(path.join(__dirname, 'dist')));
 
-server.listen(PORT, () => console.log('Welcome to Edtronics! server listening on port', PORT));
+server.listen(PORT, () => console.log(colors.info('Welcome to Edtronics! server listening on port 3001', PORT)));
 
 
 
--- src/Component/App.jsx
+++ src/Component/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+    import React, { Component } from 'react';
 import { Link } from 'react-router';
 import Aside from './Aside/Aside';
 import Footer from './Footer/Footer';
