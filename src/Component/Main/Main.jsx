f733a8cfed2e568becacfd75f6a704770877a594
mohamed gassama <bouyagassama@gmail.cpm>
2017-01-29 13:37:51 -0500

    File updated in [Edtronics] application folder

--- src/Component/Main/Main.jsx
+++ src/Component/Main/Main.jsx
@@ -3,9 +3,14 @@ import './Main.css';
 
 const Main = (props) => {
 	return (
-      <div id="main">
-      <h1>Welcome to Edtronics</h1>
-      </div>
+     <div id="main">
+       <h1>Welcome to Edtronics</h1>
+       <p>
+         Edtronics is an education technology company.
+         We specializes in designing and developing educational electronics,
+         computer hardware/softwares and deliever online services for schools, students, teachers and parents
+       </p>
+     </div>
 	);
 }
 
