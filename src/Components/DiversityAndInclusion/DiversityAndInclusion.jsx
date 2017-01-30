b985b42dda607dba4a5450c5009fac1bc58215e5
mohamed gassama <bouyagassama@gmail.cpm>
2017-01-30 14:53:09 -0500

    File updated in [Edtronics] application folder

---
+++ src/Components/DiversityAndInclusion/DiversityAndInclusion.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './DiversityAndInclusion.css';
+
+
+const DiversityAndInclusion = (props) => {
+    return (
+      <div>
+          <h1>event</h1>
+      </div>
+    );
+};
+
+
+export default DiversityAndInclusion;
