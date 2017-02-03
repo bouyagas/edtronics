0da7a68a2e8c330e0333eb79e5a3acbd134cea01
mohamed gassama <bouyagassama@gmail.cpm>
2017-02-03 09:49:34 -0500

    File updated in [Edtronics] application folder

--- src/Components/Aside/Aside.jsx
+++ src/Components/Aside/Aside.jsx
@@ -4,7 +4,7 @@ import './Aside.css';
 const Aside = (props) => {
 	return (
       <div id="aside">
-        <h1>A 2020 Vision to Education in the next two decades</h1>
+        <h1>A 2037 Vision of Education in the next two decades</h1>
         <p>The pace of change is mandating that we produce a faster, smarter, better grade of human being.
          Current systems are preventing that from happening. Future education system will be unleashed with the advent of a
         standardized rapid courseware-builder and a single point global distribution system.</p>
