8d7f4005e6b06a475bd1ec55d2771c796c9b6470
mohamed gassama <bouyagassama@gmail.cpm>
2017-04-23 12:13:12 -0400

    adding a mission statement

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -5,7 +5,7 @@ const Main = (props) => {
 	return (
      <div id="main">
        <h1>Welcome to <span>EDTRONICS</span></h1>
-        <p>The world's leading education technology company</p>
+        <p>Our mission: <br/> To empower every teacher and student throughout the world to a  <br/> high quality of education</p>
      </div>
 	);
 }
